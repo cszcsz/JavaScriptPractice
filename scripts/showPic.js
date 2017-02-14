de048c65c22e4dc14471d86ae30422e5264ccdfc
cszcsz <175837951@qq.com>
2017-02-14 23:14:30 +0800

    JS分离并向后兼容

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,8 +1,40 @@
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+addLoadEvent(prepareGallery);
+
+function prepareGallery() {
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i=0;i< links.length;i++){
+        links[i].onclick=function () {
+            return showPic(this) ? false : true;
+        }
+    }
+}
 function showPic(whichpic) {
+    if(!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
-    var placehold = document.getElementById("placeholder");
-    placehold.setAttribute("src",source);
-    var text = whichpic.getAttribute("title");
+    var placeholder = document.getElementById("placeholder");
+    if(placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src",source);
+    if(document.getElementById("description")){
+    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
     var description=document.getElementById("description");
+    if(description.firstChild.nodeType == 3){
     description.firstChild.nodeValue=text;
+     }
+    }
+    return true;
 }
