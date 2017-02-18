9b1e02bd4f9c292980ceb8b16bda88f4ff9685c1
cszcsz <175837951@qq.com>
2017-02-18 17:10:19 +0800

    no message

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -9,7 +9,33 @@ function addLoadEvent(func) {
         }
     }
 }
-addLoadEvent(prepareGallery);
+
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/csz.jpg");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
 
 function prepareGallery() {
     if(!document.getElementsByTagName) return false;
@@ -38,3 +64,5 @@ function showPic(whichpic) {
     }
     return true;
 }
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
